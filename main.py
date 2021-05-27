492613a0045f3fd87c9e22b650b27c60b015c2c3
Kevin Daley <ylivenaked@gmail.com>
2021-05-27 00:59:34 +0000

    Project scaffolding

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
